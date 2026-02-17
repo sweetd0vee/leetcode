c29fdbdd811e4164857f700ac26d8a4b9c14cc30
Arina Goloubitskaya <arinaogo@gmail.com>
2026-02-17 21:34:38 +0300

    initial commit

---
+++ 3Sum Closest.py
@@ -0,0 +1,14 @@
+# Given an integer array nums of length n and an integer target, find three integers at distinct
+# indices in nums such that the sum is closest to target.
+#
+# Return the sum of the three integers.
+#
+# You may assume that each input would have exactly one solution.
+
+
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        pass
---
+++ 41. First Missing Positive.py
@@ -0,0 +1,11 @@
+# Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+#
+# You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        pass
